8b381fb068d2e28a06ffcfe1d404c7733fe9d651
Anum Mujahid <amujahid.bese18seecs@student.nust.edu.pk>
2021-04-27 21:44:20 +0500

    feat(sidebarChat): add sidebarChat component

--- frontend/src/SidebarChat.js
+++ frontend/src/SidebarChat.js
@@ -1,7 +1,17 @@
+import { Avatar } from '@material-ui/core';
+import './SidebarChat.css';
 import React from 'react';
 
 const SidebarChat = () => {
-  return <div>sidebar chat</div>;
+  return (
+    <div className="sidebarChat">
+      <Avatar />
+      <div className="sidebarChat__info">
+        <h2>Room Name</h2>
+        <p>This is the last message</p>
+      </div>
+    </div>
+  );
 };
 
 export default SidebarChat;
