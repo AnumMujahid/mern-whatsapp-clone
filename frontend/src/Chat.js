5a39d95a15af97d9879a8d5b14463fb91b1a183b
Anum Mujahid <amujahid.bese18seecs@student.nust.edu.pk>
2021-04-27 23:27:37 +0500

    feat(chat): add chat component

--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -1,9 +1,52 @@
+import { Avatar, IconButton } from '@material-ui/core';
+import {
+  SearchOutlined,
+  AttachFile,
+  MoreVert,
+  InsertEmoticon,
+  Mic,
+} from '@material-ui/icons';
 import React from 'react';
 import './Chat.css';
 const Chat = () => {
   return (
     <div className="chat">
-      <h1>Chat</h1>
+      <div className="chat__header">
+        <Avatar />
+        <div className="chat__headerInfo">
+          <h3>Room Name</h3>
+          <p>Last seen at ...</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Anum</span>This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+        <p className="chat__message chat__receiver">
+          <span className="chat__name">Anum</span>This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+      </div>
+      <div className="chat__footer">
+        <InsertEmoticon />
+        <form>
+          <input type="text" placeholder="Type a message" />
+          <button type="submit">Send a Message</button>
+        </form>
+        <Mic />
+      </div>
     </div>
   );
 };
