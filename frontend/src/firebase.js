44d8f3905354fda67568d1d331ff3217ac949472
Anum Mujahid <amujahid.bese18seecs@student.nust.edu.pk>
2022-07-24 18:39:41 +0500

    fix(): remove secret information

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCZqEnDrLVkQoqV_cRNVoVb4-aRXRRzDmg',
-  authDomain: 'whatsapp-clone-by-anum.firebaseapp.com',
-  projectId: 'whatsapp-clone-by-anum',
-  storageBucket: 'whatsapp-clone-by-anum.appspot.com',
-  messagingSenderId: '429783564396',
-  appId: '1:429783564396:web:728cff0e96a1114a961849',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
